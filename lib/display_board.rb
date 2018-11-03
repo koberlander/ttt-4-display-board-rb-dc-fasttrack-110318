19b52f1a59d3b8ab3e9b3d61ecd8e3ba1de8d17a
Kristin M Oberlander <koberlander115@gmail.com>
2018-11-03 19:45:58 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,7 +6,7 @@ def display_board(board)
   # puts "   |   |   "
   # puts "-----------"
   # puts "   |   |   "
-  puts " #{board[0] | {board[1]}  |    "
+  puts " #{board[0]} | #{board[1]}  |    "
 end
 
 board = ["X", "O", "X", "X", "X", "X", "X", "X", "X"]
