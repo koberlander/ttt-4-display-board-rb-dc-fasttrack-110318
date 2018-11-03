bbab436120fb2d46ffd43d00d8522c1b91429ad7
Kristin M Oberlander <koberlander115@gmail.com>
2018-11-03 19:52:01 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,13 +6,13 @@ def display_board(board)
   # puts "   |   |   "
   # puts "-----------"
   # puts "   |   |   "
-  puts " #{board[0]} | #{board[1]}  | #{board[2]} "
+  puts " #{board[0]} | #{board[1]}  | #{board[2]}"
   puts "-----------"
-  puts " #{board[3]} | #{board[4]}  | #{board[5]} "
+  puts " #{board[3]} | #{board[4]}  | #{board[5]}"
   puts "-----------"
-  puts " #{board[6]} | #{board[7]}  | #{board[8]} "
+  puts " #{board[6]} | #{board[7]}  | #{board[8]}"
 end
 
-board = [" ", " ", " ", "X", " ", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 display_board(board)
